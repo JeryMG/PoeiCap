e70cde69f7db268dc271f69b1709f9f95a9bb876
Jerome Mabo <jeromemabo@gmail.com>
2022-09-09 16:27:45 +0200

    Added getName method in product page and last assertion in test

--- src/test/java/TpCarrefour.java
+++ src/test/java/TpCarrefour.java
@@ -1,7 +1,9 @@
 import carrefour.CartPage;
 import carrefour.HomePage;
+import carrefour.ProductPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,6 +11,7 @@ import org.testng.annotations.Test;
 public class TpCarrefour {
 
     WebDriver driver;
+    final String expectedProductName = "Draisienne pliable et électrique MOOVWAY";
 
     @BeforeMethod
     private void setup(){
@@ -23,15 +26,18 @@ public class TpCarrefour {
     }
 
     @Test
-    public void testCarrefour() {
+    public void testCarrefour(){
 
         //Action
         HomePage homePage = new HomePage(driver);
-        homePage.acceptCookies()
+        ProductPage productPage = homePage.acceptCookies()
                 .clickHomeHobbiesButton()
                 .OpenRayonMenu()
                 .HoverSportsLClickBSellers()
                 .ClickBestSellersButton()
                 .ClickOnFirstSportProduct();
+
+        //Assert
+        Assert.assertEquals(productPage.getProductName(), expectedProductName, "Le nom du produit est incorrect");
     }
 }
