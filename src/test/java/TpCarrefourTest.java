f2c4d2ac127e1a293fd357b00505028237a7f7e6
Jerome Mabo <jeromemabo@gmail.com>
2022-09-12 15:11:20 +0200

    Replaced selector

--- src/test/java/TpCarrefourTest.java
+++ src/test/java/TpCarrefourTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TpCarrefour {
+public class TpCarrefourTest {
 
     WebDriver driver;
     final String expectedProductName = "Draisienne pliable et électrique MOOVWAY";
