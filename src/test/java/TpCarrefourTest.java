83f464dc9f423e32a2030f83df97bb49bbcaa4b5
Jerome Mabo <jeromemabo@gmail.com>
2022-09-12 14:31:46 +0200

    circle ci config file modified

--- src/test/java/TpCarrefourTest.java
+++ src/test/java/TpCarrefourTest.java
@@ -1,5 +1,3 @@
-import carrefour.BestSellersPage;
-import carrefour.CartPage;
 import carrefour.HomePage;
 import carrefour.ProductPage;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TpCarrefour {
+public class TpCarrefourTest {
 
     WebDriver driver;
     final String expectedProductName = "Draisienne pliable et électrique MOOVWAY";
